a15504b015d6b8bcce80d03a58e1f6a3340e7b6c
BDunnCode <BDunnCode@github.com>
2023-11-18 17:14:16 -0600

    game complete

--- app.js
+++ app.js
@@ -52,8 +52,10 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
-const cardsChosen = []
-const cardsChosenIds = []
+const resultDisplay = document.querySelector('#result')
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
 
 
 const createBoard = () => {
@@ -69,14 +71,35 @@ createBoard()
 
 function checkMatch() {
     const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
     console.log(cards)
-
     console.log('check for match!')
-    if (cardsChosen[0] === cardsChosen[1])
-        alert('You found a match!')
-        cards[cardsChosenIds[0]].setAttribute('src', 'images/white.png')
-        cards[cardsChosenIds[1]].setAttribute('src', 'images/white.png')
+    if (optionOneId === optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('You have clicked the same image!')
+    }
 
+    if (cardsChosen[0] === cardsChosen[1]) {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        alert('Sorry try again!')
+    }
+    resultDisplay.textContent = cardsWon.length
+    cardsChosen = []
+    cardsChosenIds = []
+
+    if(cardsWon.length === (cardArray.length/2)) {
+        resultDisplay.textContent = 'Congratulations you found them all!'
+    }
 }
 
 function flipCard (){
