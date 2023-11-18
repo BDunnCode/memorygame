45ce7efdb1737499121c07fc6e4204f287ce72b8
BDunnCode <BDunnCode@github.com>
2023-11-18 14:04:35 -0600

    next commit

--- app.js
+++ app.js
@@ -52,3 +52,20 @@ const cardArray = [
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+
+
+const createBoard = () => {
+    for(let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        gridDisplay.appendChild(card)
+    }
+}
+createBoard()
+
+function flipCard (){
+    const cardId = this.getAttribute('data-id')
+    console.log('clicked', cardId)
+}
